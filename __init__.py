efd95a08b799037e5d8d0a9a4058c4b0236f4997
Serge Matveenko <s@matveenko.ru>
2008-12-06 01:01:49 +0300

    alpha functionality caching works

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import configobj
 import gzip
 import sys
@@ -162,20 +163,173 @@ class _StarDictIdx():
         return self._idx[word]
 
 class _StarDictDict():
-    #TODO: implement .dict special methods
+    """
+    The .dict file is a pure data sequence, as the offset and size of each
+    word is recorded in the corresponding .idx file.
+    
+    If the "sametypesequence" option is not used in the .ifo file, then
+    the .dict file has fields in the following order:
+    ==============
+    word_1_data_1_type; // a single char identifying the data type
+    word_1_data_1_data; // the data
+    word_1_data_2_type;
+    word_1_data_2_data;
+    ...... // the number of data entries for each word is determined by
+           // word_data_size in .idx file
+    word_2_data_1_type;
+    word_2_data_1_data;
+    ......
+    ==============
+    It's important to note that each field in each word indicates its
+    own length, as described below.  The number of possible fields per
+    word is also not fixed, and is determined by simply reading data until
+    you've read word_data_size bytes for that word.
+    
+    
+    Suppose the "sametypesequence" option is used in the .idx file, and
+    the option is set like this:
+    sametypesequence=tm
+    Then the .dict file will look like this:
+    ==============
+    word_1_data_1_data
+    word_1_data_2_data
+    word_2_data_1_data
+    word_2_data_2_data
+    ......
+    ==============
+    The first data entry for each word will have a terminating '\0', but
+    the second entry will not have a terminating '\0'.  The omissions of
+    the type chars and of the last field's size information are the
+    optimizations required by the "sametypesequence" option described
+    above.
+    
+    If "idxoffsetbits=64", the file size of the .dict file will be bigger 
+    than 4G. Because we often need to mmap this large file, and there is 
+    a 4G maximum virtual memory space limit in a process on the 32 bits 
+    computer, which will make we can get error, so "idxoffsetbits=64" 
+    dictionary can't be loaded in 32 bits machine in fact, StarDict will 
+    simply print a warning in this case when loading. 64-bits computers 
+    should haven't this limit.
+    
+    Type identifiers
+    ----------------
+    Here are the single-character type identifiers that may be used with
+    the "sametypesequence" option in the .idx file, or may appear in the
+    dict file itself if the "sametypesequence" option is not used.
+    
+    Lower-case characters signify that a field's size is determined by a
+    terminating '\0', while upper-case characters indicate that the data
+    begins with a network byte-ordered guint32 that gives the length of 
+    the following data's size(NOT the whole size which is 4 bytes bigger).
+    
+    'm'
+    Word's pure text meaning.
+    The data should be a utf-8 string ending with '\0'.
+    
+    'l'
+    Word's pure text meaning.
+    The data is NOT a utf-8 string, but is instead a string in locale
+    encoding, ending with '\0'.  Sometimes using this type will save disk
+    space, but its use is discouraged.
+    
+    'g'
+    A utf-8 string which is marked up with the Pango text markup language.
+    For more information about this markup language, See the "Pango
+    Reference Manual."
+    You might have it installed locally at:
+    file:///usr/share/gtk-doc/html/pango/PangoMarkupFormat.html
+    
+    't'
+    English phonetic string.
+    The data should be a utf-8 string ending with '\0'.
+    
+    Here are some utf-8 phonetic characters:
+    θʃŋʧðʒæıʌʊɒɛəɑɜɔˌˈːˑṃṇḷ
+    æɑɒʌәєŋvθðʃʒɚːɡˏˊˋ
+    
+    'x'
+    A utf-8 string which is marked up with the xdxf language.
+    See http://xdxf.sourceforge.net
+    StarDict have these extention:
+    <rref> can have "type" attribute, it can be "image", "sound", "video" 
+    and "attach".
+    <kref> can have "k" attribute.
+    
+    'y'
+    Chinese YinBiao or Japanese KANA.
+    The data should be a utf-8 string ending with '\0'.
+    
+    'k'
+    KingSoft PowerWord's data. The data is a utf-8 string ending with '\0'.
+    It is in XML format.
+    
+    'w'
+    MediaWiki markup language.
+    See http://meta.wikimedia.org/wiki/Help:Editing#The_wiki_markup
+    
+    'h'
+    Html codes.
+    
+    'r'
+    Resource file list.
+    The content can be:
+    img:pic/example.jpg     // Image file
+    snd:apple.wav           // Sound file
+    vdo:film.avi            // Video file
+    att:file.bin            // Attachment file
+    More than one line is supported as a list of available files.
+    StarDict will find the files in the Resource Storage.
+    The image will be shown, the sound file will have a play button.
+    You can "save as" the attachment file and so on.
+    
+    'W'
+    wav file.
+    The data begins with a network byte-ordered guint32 to identify the wav
+    file's size, immediately followed by the file's content.
+    
+    'P'
+    Picture file.
+    The data begins with a network byte-ordered guint32 to identify the picture
+    file's size, immediately followed by the file's content.
+    
+    'X'
+    this type identifier is reserved for experimental extensions.
+    """
     
     def __init__(self, dict_prefix, container):
+        """
+        opens regular or dziped .dict file 
+        """
+        self._container = container
         
         dict_filename = '%s.dict' % dict_prefix
         dict_filename_dz = '%s.dz' % dict_filename
         
         try:
-            self._file = open(dict_filename)
+            self._file = open(dict_filename, 'rb')
         except IOError:
             try:
-                self._file = gzip.open(dict_filename_dz)
+                self._file = gzip.open(dict_filename_dz, 'rb')
             except IOError:
                 raise Exception('.dict file does not exists')
+    
+    def __getitem__(self, word):
+        """
+        returns data from .dict for word
+        """
+        
+        # getting word data coordinats
+        cords = self._container.idx[word]
+        
+        # seeking in file for data
+        self._file.seek(cords[0])
+        
+        # reading data
+        bytes = self._file.read(cords[1])
+        
+        #TODO: handle multifield data
+        #TODO: handle encoding. it seems to be working as is but i'm not sure 
+        return bytes
 
 class _StarDictSyn():
     #TODO: implement .syn special methods
@@ -229,6 +383,9 @@ class Dictionary(dict):
         
         # reading somedict.syn (optional)
         self.syn = _StarDictSyn(dict_prefix=filename_prefix, container=self)
+        
+        # initializing cache
+        self._dict_cache = {}
     
     def __cmp__(self, y):
         """
@@ -246,7 +403,7 @@ class Dictionary(dict):
     
     def __delitem__(self, k):
         """
-        blacklists dictionary entry for the word k
+        frees cache from word k translation
         
         TODO: implement me
         """
@@ -254,11 +411,9 @@ class Dictionary(dict):
     
     def __eq__(self, y):
         """
-        returns True if md5(x.idx) is equal to md5(y.idx), else False
-        
-        TODO: implement me
+        raises NotImplemented exception
         """
-        pass
+        raise NotImplementedError()
     
     def __ge__(self, y):
         """
@@ -268,11 +423,14 @@ class Dictionary(dict):
     
     def __getitem__(self, k):
         """
-        returns translation for word k
-        
-        TODO: implement me
+        returns translation for word k from cache or not and then caches
         """
-        pass
+        try:
+            return self._dict_cache[k]
+        except KeyError:
+            value = self.dict[k]
+            self._dict_cache[k] = value
+            return value
     
     def __gt__(self, y):
         """
--- test.py
+++ test.py
@@ -1,10 +1,38 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 from __init__ import Dictionary
 
 if __name__ == '__main__':
-
-    dict1 = Dictionary('/home/lig/workspace/PyStarDict/stardict-quick_eng-rus-2.4.2/quick_english-russian')
-    dict2 = Dictionary('/home/lig/workspace/PyStarDict/stardict-quick_rus-eng-2.4.2/quick_russian-english')
-
+    
+    milestone1 = datetime.datetime.today()
+    
+    dict1 = Dictionary('stardict-quick_eng-rus-2.4.2/quick_english-russian')
+    dict2 = Dictionary('stardict-quick_rus-eng-2.4.2/quick_russian-english')
+    
+    milestone2 = datetime.datetime.today()
+    print '2 dicts load:', milestone2-milestone1
+    
     print dict1.idx['test']
     print dict2.idx['аббат']
+    
+    milestone3 = datetime.datetime.today()
+    print '2 cords getters:', milestone3-milestone2
+    
+    print dict1.dict['test']
+    print dict2.dict['аббат']
+    
+    milestone4 = datetime.datetime.today()
+    print '2 direct data getters (w\'out cache):', milestone4-milestone3
+    
+    print dict1['test']
+    print dict2['аббат']
+
+    milestone5 = datetime.datetime.today()
+    print '2 high level data getters (not cached):', milestone5-milestone4
+    
+    print dict1['test']
+    print dict2['аббат']
+    
+    milestone6 = datetime.datetime.today()
+    print '2 high level data getters (cached):', milestone6-milestone5
